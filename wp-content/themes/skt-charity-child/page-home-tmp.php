4ab17a76bfedbe7c103c87d612b9dbc66d70385a
Shapuleto <shapuleto@gmail.com>
2016-11-14 14:32:11 +0700

    Install SKT Charity Theme & Child

---
+++ wp-content/themes/skt-charity-child/page-home-tmp.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template Name: Page Old Site
+ *
+ * @package WordPress
+ * @subpackage SKT Child
+ * @since SKT Child 1.0
+ *
+ * @Description Copy personilazed of page.php template
+ *
+ */
+?>
+<?php
+require_once "old/index.php";
+?>
